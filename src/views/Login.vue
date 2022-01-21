f929eb1ecf30a6c7531c3b3d418afa9495c8535b
Zehuan Huang <zewanhuang@126.com>
2022-01-21 15:02:25 +0800

    feat(login): init and add router

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="login">
+    <form class="form-box">
+      <h1>Login</h1>
+      <label><input v-model="username" type="text" placeholder="Username"></label>
+      <label><input v-model="password" type="password" placeholder="Password"></label>
+      <button type="submit" @click="click_login">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    click_login() {
+      window.alert(this.username + this.password);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
